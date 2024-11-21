addcdac9dbfc29f580efa44002ed6f61508ee045
leko94 <ngcobo.nkululeko@yahoo.com>
2024-11-22 01:07:48 +0200

    FIRE.py

--- FIRE.py
+++ FIRE.py
@@ -72,7 +72,7 @@ app.layout = html.Div([
              }),
 
     # Add the gauge chart, modify to fit the full width
-    dcc.Graph(figure=gauge1, style={'width': '100%', 'height': '400px', 'margin': '0', 'padding': '0'}),
+    dcc.Graph(figure=gauge1, style={'width': '200%', 'height': '400px', 'margin': '0', 'padding': '0'}),
 ])
 
 # Run the Dash app
